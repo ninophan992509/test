fcf42c5fb7b5d8da26316cf054600a58d1701c07
ptkngan <ptkngan7@gmail.com>
2020-09-27 13:26:55 +0700

    thirst commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express= require("express");
 var app = express();
 
 var server= require("http").createServer(app);
-server.listen(3000);
+server.listen(process.env.PORT||3000);
 
 app.get("/",function(req,res){
 
